fb54c7464bb6bf8a769c2bbd91a479b7c4b5ca7e
straywithsmile <straywithsmile@163.com>
2015-01-25 14:30:23 +0800

    µ÷Õû³õʼ»¯

--- initial.sh
+++ initial.sh
@@ -9,6 +9,8 @@ ln -sv ${pwd}/start.sh ${home}/start.sh
 ln -sv ${pwd}/stop.sh ${home}/stop.sh
 ln -sv ${pwd}/vim ${home}/.vim
 ln -sv ${pwd}/vimrc ${home}/.vimrc
+ln -sv ${pwd}/zprezto ${home}/.vimrc
+zsh ./stand_init.zsh
 
 mkdir -p ${home}/tx2env
 touch ${home}/tx2env/host.conf

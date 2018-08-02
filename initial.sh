a22a48ee9aff7d2de667816c951cc956a4abeddc
straywithsmile@163.com <straywithsmile@163.com>
2018-08-02 21:37:57 +0800

    remove zsh

--- initial.sh
+++ initial.sh
@@ -1,23 +1,9 @@
 #/usr/bin/env sh
 
-pwd=$PWD
-home=$HOME
-env_dir=${home}/work_conf
+HERE=$PWD
 
-ln -sv ${env_dir}/bin ${home}/bin
-ln -sv ${env_dir}/bash_profile ${home}/.bash_profile
-ln -sv ${env_dir}/start.sh ${home}/start.sh
-ln -sv ${env_dir}/stop.sh ${home}/stop.sh
-ln -sv ${env_dir}/vim ${home}/.vim
-ln -sv ${env_dir}/vimrc ${home}/.vimrc
-ln -sv ${env_dir}/zprezto ${home}/.zprezto
-ln -sv ${env_dir}/autojump ${home}/.autojump 
-ln -sv ${env_dir}/tmux.conf ${home}/.tmux.conf
-ln -sv ${env_dir}/teamocil ${home}/.teamocil
-
-zsh ./stand_init.zsh
-
-mkdir -p ${home}/tx2env
-touch ${home}/tx2env/host.conf
-echo "use=unknown" >> ${home}/tx2env/internal_server_info.txt
-echo "desc=unknown" >> ${home}/tx2env/internal_server_info.txt
+ln -sv ${HERE}/bin ${HOME}/bin
+ln -sv ${HERE}/bash_profile ${HOME}/.bash_profile
+ln -sv ${HERE}/vimrc ${HOME}/.vimrc
+ln -sv ${HERE}/autojump ${HOME}/.autojump 
+ln -sv ${HERE}/tmux.conf ${HOME}/.tmux.conf

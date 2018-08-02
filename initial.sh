130c7f109443a4861ac37eb1e427e067cd30d566
straywithsmile@163.com <straywithsmile@163.com>
2018-08-02 21:55:36 +0800

    add initial

--- initial.sh
+++ initial.sh
@@ -7,3 +7,5 @@ ln -sv ${HERE}/bash_profile ${HOME}/.bash_profile
 ln -sv ${HERE}/vimrc ${HOME}/.vimrc
 ln -sv ${HERE}/autojump ${HOME}/.autojump 
 ln -sv ${HERE}/tmux.conf ${HOME}/.tmux.conf
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

6a0b603d8ab9ae4c3b1f086681d6618b89f7fb62
straywithsmile@163.com <straywithsmile@163.com>
2018-08-02 21:44:11 +0800

    modify initial

--- initial.sh
+++ initial.sh
@@ -2,7 +2,7 @@
 
 HERE=$PWD
 
-ln -sv ${HERE}/bin ${HOME}/bin
+ln -sv ${HERE}/bin ${HOME}/.bin
 ln -sv ${HERE}/bash_profile ${HOME}/.bash_profile
 ln -sv ${HERE}/vimrc ${HOME}/.vimrc
 ln -sv ${HERE}/autojump ${HOME}/.autojump 
